bc0915c25b60f5356aa38ca7a2eee0ff37ca6a1d
Lee So Yeon <panda_g02@naver.com>
2026-08-01 22:02:08 +0900

    [Passed] 반복 출력하기 (2ms, 0MB)

---
+++ trail2/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main() {
+    cin >> N;
+
+    for(int i = 0 ; i < N; i++){
+        cout << "12345^&*()_" << endl;
+    }
+
+    return 0;
+}
